fe6131e972d59e12e4c10b533070143fa177e1f0
ooiyeefei <yeefeiooi@gmail.com>
2025-05-16 10:57:44 +0800

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,4 +1,18 @@
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+  alias = "eks_cluster"
+}
+
+
 resource "kubernetes_storage_class" "ebs_gp3" {
+  provider = kubernetes.eks_cluster
+
   metadata {
     name = "ebs-csi-gp3"
     annotations = {
