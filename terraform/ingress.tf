b6b4a507c7b787dd428727b121c059e1042da28c
ooiyeefei <yeefeiooi@gmail.com>
2025-05-17 11:38:40 +0800

    Update ingress.tf

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -91,11 +91,11 @@ resource "kubernetes_manifest" "patch_rafay_ingress" {
       namespace = "vcluster-small"
       annotations = {
         "kubernetes.io/ingress.class"                 = "alb"
-        "alb.ingress.kubernetes.io/healthcheck-path"  = "/api"
+        "alb.ingress.kubernetes.io/healthcheck-path"  = "/api/health"
         "alb.ingress.kubernetes.io/healthcheck-port"  = "8888"
         "alb.ingress.kubernetes.io/target-type"       = "ip"
         "alb.ingress.kubernetes.io/listen-ports"      = "[{\"HTTP\": 80}]"
-        "alb.ingress.kubernetes.io/success-codes"     = "200-399"
+        "alb.ingress.kubernetes.io/success-codes"     = "399,404"
         "cert-manager.io/cluster-issuer"              = "letsencrypt-demo"
         "kubernetes.io/tls-acme"                      = "true"
         "nginx.ingress.kubernetes.io/proxy-body-size" = "50m"
