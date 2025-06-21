1c82e947bd5eb2c3359362f405d43e5d93acc3cb
Ooi Yee Fei <yooi@Oois-MacBook-Air.local>
2025-06-21 11:46:13 +0800

    fix terraform error unsupported block type with set

--- terraform/external-secrets.tf
+++ terraform/external-secrets.tf
@@ -50,21 +50,20 @@ resource "helm_release" "external_secrets" {
   namespace  = "external-secrets"
   create_namespace = true
   
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-
-  # Create the service account without annotations for Pod Identity
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-  
-  set {
-    name  = "serviceAccount.name"
-    value = "external-secrets-sa"
-  }
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "external-secrets-sa"
+    }
+  ]
   
   # Removed service account annotation as it's not needed for Pod Identity
 
