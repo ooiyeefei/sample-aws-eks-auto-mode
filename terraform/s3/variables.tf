d1a134917c95b8ccf09301194578e8e5463e404e
Ooi Yee Fei <yooi@Oois-MacBook-Air.local>
2025-06-22 21:06:51 +0800

    edit s3 module

--- terraform/s3/variables.tf
+++ terraform/s3/variables.tf
@@ -22,10 +22,4 @@ variable "vpc_id" {
   description = "VPC ID (not used in S3 module but passed by Rafay)"
   type        = string
   default     = ""
-}
-
-variable "openwebui_pod_identity_role_name" {
-  description = "The name of the IAM role for OpenWebUI Pod Identity (output from this module)"
-  type        = string
-  default     = ""
 } 
