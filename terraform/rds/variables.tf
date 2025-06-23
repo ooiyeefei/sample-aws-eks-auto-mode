29b1ab782e821797b7bc6e8ffb435304aa0c6f54
Ooi Yee Fei <yooi@Oois-MacBook-Air.local>
2025-06-23 16:28:34 +0800

    remove validation block in vpc_cidr

--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -16,11 +16,6 @@ variable "vpc_id" {
 variable "vpc_cidr" {
   description = "VPC CIDR block for security group rules. This should be wired from the EKS module's output."
   type        = string
-
-  validation {
-    condition     = can(cidrnet(var.vpc_cidr, 0))
-    error_message = "The provided vpc_cidr is not a valid CIDR block. This usually happens if the platform fails to substitute the expression with its real value."
-  }
 }
 
 variable "private_subnets" {
