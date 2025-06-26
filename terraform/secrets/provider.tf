c2c8050c20204e639efacab8da7cfd6296666b49
Ooi Yee Fei <yooi@Oois-MacBook-Air.local>
2025-06-26 23:13:29 +0800

    update secrets to auth correctly into cluster

--- terraform/secrets/provider.tf
+++ terraform/secrets/provider.tf
@@ -22,7 +22,7 @@ provider "kubernetes" {
 
 # Configure the Helm provider to use the same dynamic data
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
