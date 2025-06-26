efe83e3a92eb8989f3e7e1ebb910a3b37d078c57
Ooi Yee Fei <yooi@Oois-MacBook-Air.local>
2025-06-26 23:00:31 +0800

    update secrets to auth correctly into cluster

--- terraform/secrets/provider.tf
+++ terraform/secrets/provider.tf
@@ -2,16 +2,29 @@ provider "aws" {
   region = var.region
 }
 
-provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-  token                  = var.kube_token
+# Data source to get information about the EKS cluster by name
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
 }
 
+# Data source to generate a temporary authentication token for the cluster
+# This uses the AWS credentials provided by the Rafay Environment's Config Context
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+# Configure the Kubernetes provider to use the dynamic data
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# Configure the Helm provider to use the same dynamic data
 provider "helm" {
-  kubernetes = {
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-    token                  = var.kube_token
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
-} 
+}
