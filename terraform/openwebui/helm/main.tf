aaf768ef20d0c3dd2f84160c525be706ea022c35
Ooi Yee Fei <yeefeiooi@gmail.com>
2025-06-29 22:23:58 +0800

    fix kubeconfig needed

--- terraform/openwebui/helm/main.tf
+++ terraform/openwebui/helm/main.tf
@@ -112,19 +112,53 @@ resource "rafay_workload" "openwebui_load_balancer" {
   }
 }
 
+resource "local_sensitive_file" "kubeconfig" {
+  content = yamlencode({
+    apiVersion      = "v1"
+    kind            = "Config"
+    current-context = "default"
+    clusters = [{
+      name = var.cluster_name
+      cluster = {
+        server                   = data.aws_eks_cluster.cluster.endpoint
+        certificate-authority-data = data.aws_eks_cluster.cluster.certificate_authority[0].data
+      }
+    }]
+    contexts = [{
+      name = "default"
+      context = {
+        cluster = var.cluster_name
+        user    = "default"
+      }
+    }]
+    users = [{
+      name = "default"
+      user = {
+        token = data.aws_eks_cluster_auth.cluster.token
+      }
+    }]
+  })
+  filename = "/tmp/kubeconfig"
+}
+
+
 resource "time_sleep" "wait_for_lb_provisioning" {
   depends_on      = [rafay_workload.openwebui_load_balancer]
   create_duration = "90s"
 }
 
 resource "null_resource" "get_lb_hostname" {
-  depends_on = [time_sleep.wait_for_lb_provisioning]
+  depends_on = [
+    time_sleep.wait_for_lb_provisioning,
+    local_sensitive_file.kubeconfig
+  ]
 
   provisioner "local-exec" {
-    command = "kubectl get service open-webui-service -n ${var.namespace} -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' > /tmp/lb_hostname.txt"
+    command = "kubectl --kubeconfig /tmp/kubeconfig get service open-webui-service -n ${var.namespace} -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' > /tmp/lb_hostname.txt"
   }
 }
 
+# Step 4: Read the result from the output file.
 data "local_file" "load_balancer_hostname" {
   depends_on = [null_resource.get_lb_hostname]
   filename   = "/tmp/lb_hostname.txt"
