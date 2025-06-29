f40e7f709843c9fd82a31e9762159382de9643c4
Ooi Yee Fei <yeefeiooi@gmail.com>
2025-06-29 19:53:28 +0800

    update format for attribute

--- terraform/openwebui/helm/outputs.tf
+++ terraform/openwebui/helm/outputs.tf
@@ -1,9 +1,9 @@
 output "load_balancer_hostname" {
   description = "The raw hostname of the AWS Network Load Balancer."
-  value       = data.kubernetes_service.openwebui_lb.status[0].load_balancer[0].ingress[0].hostname
+  value       = data.kubernetes_service.openwebui_lb.status.0.load_balancer.0.ingress.0.hostname
 }
 
 output "openwebui_access_url" {
   description = "The full URL to access the OpenWebUI application."
-  value       = "http://${data.kubernetes_service.openwebui_lb.status[0].load_balancer[0].ingress[0].hostname}"
+  value       = "http://${data.kubernetes_service.openwebui_lb.status.0.load_balancer.0.ingress.0.hostname}"
 }
